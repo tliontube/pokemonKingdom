3d6639eb8506dcd48f70a0e79e742ed7a1f3cc33
tliontube <thomaswizfan@gmail.com>
2023-09-19 23:01:59 +0530

    v3

--- src/_app.js
+++ src/_app.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import "./App.css";
-import Header from "./Components/Header";
-import PokemonContainer from './Components/PokemonContainer';
+
 
 export function App() {
  
     return(
         <div id="parent">
-                <Header/>
-                <PokemonContainer/>
+
         </div>
     )
     
